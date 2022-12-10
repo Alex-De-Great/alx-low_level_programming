90c7496af8858e6943b59233a2a8b576c448551e
Alex-De-Great <winsdor20@gmail.com>
2022-12-10 00:48:42 -0800

    Tasks 4 and 5

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -5,8 +5,4 @@
  *
  * Return: Always 0 (Sucess)
  */
-int main(void){
-	puts("\"Programming is like building a multilingual puzzle");
-	return (0);
-
-}
+int
---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+
+/*
+ * To execute a prinf statement 
+ */
+
+int main(void) {
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
