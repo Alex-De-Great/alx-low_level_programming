5960d61b4c9ba79da5b2008838aba382978cc38e
Alex-De-Great <winsdor20@gmail.com>
2022-12-10 05:17:27 -0800

    Task 4 and 5

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -5,10 +5,6 @@
  *
  * Return: Always 0 (Sucess)
  */
-int main(void) 
-{
-	puts("\"Programming is like building a multilingual puzzle");
-
+int main(void){					puts("\"Programming is like building a multilingual puzzle");
 	return (0);
-
-}
+	}
