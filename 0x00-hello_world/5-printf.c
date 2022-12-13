3c5a30ba3ef34f0175559a799cb45d2780b34ffb
Alex-De-Great <winsdor20@gmail.com>
2022-12-13 07:35:03 -0800

    Task 5 and 6

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
-/*
- * Main - Entry Point
- * Description: print ("with proper grammer,but the outcome is a piece of art,")
- * Return: Always 0 (Success)
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (success)
  */
 int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art,\n");
+	printf("with proper grammer, but the outcome is a piece of art,\n");
 	return (0);
 }
--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,17 +1,15 @@
-#include <stdio.h>
-/*
- * Main - Entry Point
+# include <stdio.h>
+/**
+ * main - Entry point
  *
- *Description: print (the size of various types on the computer)
-
- * Return : Always 0 (Success)
+ * Return: Always 0
  */
 int main(void)
 {
 	printf("Size of a char: %tu byte(s)\n", sizeof(char));
-	printf("Size of an int: %tu byte(s)\n", sizeof(int));
-	printf("Size of a long int: %tu byte(s)\n", sizeof(long int));
-	printf("Size of a long long int: %tu byte(s)\n", sizeof(long long int));
-	printf("Size of a float: %tu byte(s)\n", sizeof(float));
+	printf("Size of an int: %tu byte(s)\n", sizeof(char));
+	printf("Size of a long int: %tu byte(s)\n", sizeof(char));
+	printf("Size of a long long int: %tu byte(s)\n", sizeof(char));
+	printf("Size of a float: %tu byte(s)\n", sizeof(char));
 	return (0);
 }
