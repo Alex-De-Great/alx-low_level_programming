56c82ce4f7d6c231216acb6d6bcd4684ad636cfd
Alex-De-Great <winsdor20@gmail.com>
2023-08-13 16:59:30 -0700

    task 5

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
 /**
  * main - entry point
- * 
- * return: Always 0 (success)
+ *
+ * return:Always 0 (success)
  */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle\n");
+
 	return (0);
 }
